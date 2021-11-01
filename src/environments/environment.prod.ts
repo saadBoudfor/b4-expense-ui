727d44bbe5957154452c572d3520d864aa36388b
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2021-11-01 12:17:36 +0100

    add correct image server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://apps.boudfor.fr/api',
-  fileServerURL: 'http://localhost:8887',
+  fileServerURL: 'https://apps.boudfor.fr/data',
   appVersion: '1.0.0'
 };
