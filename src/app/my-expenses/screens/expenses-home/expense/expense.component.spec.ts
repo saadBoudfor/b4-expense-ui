269de21447669bb756a2100102ac45ae6efd6bc9
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2022-04-22 23:44:24 +0200

    add pipe tests

--- src/app/my-expenses/screens/expenses-home/expense/expense.component.spec.ts
+++ src/app/my-expenses/screens/expenses-home/expense/expense.component.spec.ts
@@ -1,19 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
-import { ExpenseComponent } from './expense.component';
+import {ExpenseComponent} from './expense.component';
+import {StorageRepository} from "../../../../b4-storages/repositories/storage-repository.service";
+import {ConfigService} from "../../../../b4-common/services/config.service";
+import {of} from "rxjs";
 
-describe('ExpenseComponent', () => {
+fdescribe('ExpenseComponent', () => {
   let component: ExpenseComponent;
   let fixture: ComponentFixture<ExpenseComponent>;
-
+  let configServiceMock = jasmine.createSpyObj(['getSelectedTheme']);
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExpenseComponent ]
+      declarations: [ExpenseComponent],
+      providers: [
+        {provide: ConfigService, useValue: configServiceMock}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
+    configServiceMock.getSelectedTheme.and.returnValue(of('light'));
     fixture = TestBed.createComponent(ExpenseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +29,17 @@ describe('ExpenseComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update dark value if light theme selected', fakeAsync(() => {
+    expect(component.isDark).toBeFalse();
+  }))
+
+  it('should update dark value if dark theme selected', fakeAsync(() => {
+    configServiceMock.getSelectedTheme.and.returnValue(of('dark-theme'));
+    fixture = TestBed.createComponent(ExpenseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.isDark).toBeTruthy();
+  }))
 });
