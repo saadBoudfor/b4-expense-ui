abb17bb96227877668d95a9bb6f43e5f452f1800
Saâd BOUDFOR <saad.boudfor.externe@odigo.com>
2022-05-07 10:22:46 +0200

    add unit tests to add expense list components

--- src/app/my-expenses/screens/expense-list/display-household-expense-list/display-household-expense-list.component.ts
+++ src/app/my-expenses/screens/expense-list/display-household-expense-list/display-household-expense-list.component.ts
@@ -3,6 +3,7 @@ import {ExpenseService} from "../../../services/expense.service";
 import {NGXLogger} from "ngx-logger";
 import {ExpenseLine} from "../../../models/ExpenseLine";
 import {Expense} from "../../../models/Expense";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'display-household-expense-list',
@@ -16,6 +17,7 @@ export class DisplayHouseholdExpenseListComponent implements OnInit {
   productName = '';
 
   constructor(private expenseService: ExpenseService,
+              private snackBar: MatSnackBar,
               private logger: NGXLogger) {
   }
 
@@ -24,19 +26,23 @@ export class DisplayHouseholdExpenseListComponent implements OnInit {
     this.expense.expenseLines = [];
     this.expenseLines = this.expense.expenseLines;
 
-    this.logger.info('load component');
+    this.logger.info('load display household expense list');
     const loadedExpense = this.expenseService.getCurrentDraft();
     if (!!loadedExpense) {
+      this.logger.info('load saved draft expense from cache success');
       this.expense = loadedExpense;
       this.expenseLines = this.expense.expenseLines;
+    } else {
+      const message = 'failed to load saved draft expense from cache';
+      this.snackBar.open(message);
+      this.logger.error(message);
     }
   }
 
   save() {
+    this.logger.info('update household expense list', {expense: this.expense});
     this.expenseService.updateDraft(this.expense);
   }
 
 
 }
-
-const logId = '[DisplayHouseholdExpenseListComponent] ';
